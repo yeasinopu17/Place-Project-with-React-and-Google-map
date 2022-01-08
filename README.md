ce2fc241a301d1e1797b098f585069a8b74be05f
yeasinopu17 <yeasinopu@gmail.com>
2022-01-08 23:39:03 +0600

    with openstreet map

---
+++ README.md
@@ -0,0 +1,4 @@
+# React, NodeJS, Express & MongoDB - The MERN Fullstack Guide
+## Instructor: Maximilian Schwarzmüller
+## Place Project with React and Google map
+![App ScreenShot](https://github.com/yeasinopu17/place-Project-with-React-and-Google-map/blob/master/Screenshot.jpg)
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -2,27 +2,27 @@ import React, { useRef, useEffect } from 'react';
 
 import './Map.css';
 
-const Map = props => {
-  const mapRef = useRef();
-  
-  const { center, zoom } = props;
+const Map = (props) => {
+    const mapRef = useRef();
 
-  useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom
-    });
-  
-    new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
+    const { center, zoom } = props;
 
-  return (
-    <div
-      ref={mapRef}
-      className={`map ${props.className}`}
-      style={props.style}
-    ></div>
-  );
+    useEffect(() => {
+        new window.ol.Map({
+            target: mapRef.current.id,
+            layers: [
+                new window.ol.layer.Tile({
+                    source: new window.ol.source.OSM(),
+                }),
+            ],
+            view: new window.ol.View({
+                center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+                zoom: zoom,
+            }),
+        });
+    }, [center, zoom]);
+
+    return <div ref={mapRef} className={`map ${props.className}`} style={props.style} id="map"></div>;
 };
 
 export default Map;
